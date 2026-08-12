6a380140b126942e7649e8bf23a5ef4a5e304a47
AceofJesters <ems.nelson@hotmail.com>
2026-08-12 10:56:19 -0600

    basic movement and monster/character interaction setup

---
+++ Monster.py
@@ -0,0 +1,17 @@
+class Monster:
+
+    def __init__(self, pos=(0, 0)):
+        self.pos = pos
+
+    def move(self, dir):
+        dx, dy = 0, 0
+        if dir == "up":
+            dy = -50
+        elif dir == "down":
+            dy = 50
+        elif dir == "left":
+            dx = -50
+        elif dir == "right":
+            dx = 50
+        self.pos = (self.pos[0] + dx, self.pos[1] + dy)
+        return f"Monster moved to {self.pos}"
---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame
+from Character import Character
+from Monster import Monster
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("My Game")
+
+Ezekiel = Character(pos=(100, 100))
+Monster = Monster(pos=(400, 300))
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print(Ezekiel.move("up"))
+            elif event.key == pygame.K_s:
+                print(Ezekiel.move("down"))
+            elif event.key == pygame.K_a:
+                print(Ezekiel.move("left"))
+            elif event.key == pygame.K_d:
+                print(Ezekiel.move("right"))
+            elif event.key == pygame.K_c:
+                running = False
+
+        
+        if Ezekiel.pos == Monster.pos:
+            print("Ezekiel has encountered the Monster!")
+
+    screen.fill((255/2, 255/2, 255/2))
+    pygame.draw.rect(screen, (0, 0, 225), (Ezekiel.pos[0], Ezekiel.pos[1], 50, 50))
+    pygame.draw.rect(screen, (225, 0, 0), (Monster.pos[0], Monster.pos[1], 50, 50))
+
+    pygame.display.update()
+
+pygame.quit()
