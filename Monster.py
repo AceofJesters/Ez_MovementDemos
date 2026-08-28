17bc3875de937a5a9cf13b8f0cd469e0e2fad349
AceofJesters <ems.nelson@hotmail.com>
2026-08-28 14:56:12 -0600

    begin designing monster pathfinding

--- Monster.py
+++ Monster.py
@@ -4,6 +4,7 @@ class Monster:
 
     def __init__(self, pos=(0, 0)):
         self.pos = pos
+        self.dest = pos
 
     def move(self, dir):
         old_pos = self.pos
@@ -22,10 +23,38 @@ class Monster:
         if self.pos[1] not in range(0, 600 - 50):
             self.pos = (self.pos[0], old_pos[1])
         return f"Monster moved to {self.pos}"
-    
-    def select_destination(self, SCREEN_WIDTH=800, SCREEN_HEIGHT=600):
-        wbound = SCREEN_WIDTH - 50
-        hbound = SCREEN_HEIGHT - 50
 
+    
+    def pathfind(self, target_pos):
+        if self.pos[0] < target_pos[0]:
+            return "right"
+        elif self.pos[0] > target_pos[0]:
+            return "left"
+        elif self.pos[1] < target_pos[1]:
+            return "down"
+        elif self.pos[1] > target_pos[1]:
+            return "up"
+        else:
+            return None
+
+
+    def find_direction(self):
         dir = random.choice(["up", "down", "left", "right"])
         return dir
+
+    
+    def select_destination(self, SCREEN_WIDTH=800, SCREEN_HEIGHT=600):
+
+        if self.pos != self.dest:
+            return self.dest
+
+        else:
+
+            wbound = SCREEN_WIDTH - 50
+            hbound = SCREEN_HEIGHT - 50
+
+            destx = random.randint(0, wbound)
+            desty = random.randint(0, hbound)
+
+            dir = self.find_direction()
+            return dir
