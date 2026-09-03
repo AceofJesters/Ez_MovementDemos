318b8f059e261bc7975bc875ed50770a5e8d8e69
AceofJesters <ems.nelson@hotmail.com>
2026-09-03 10:09:05 -0600

    add core open/closed list logic

--- pathfind.py
+++ pathfind.py
@@ -11,12 +11,17 @@ def pathfind(start, goal):
         # add each neighbour of current cell to open list w/ current node saved as parent
         neighbours_coords = get_neighbours(current_node.coords)
         # - for each neighbour
+        for neighbour in neighbours_coords:
             # 	- ensure it's not on the open/closed list or a wall
+            if neighbour not in open_list and neighbour not in closed_list and neighbour not in walls:
             # 	- create new node with current node as parent, heuristic calculation, parent cost, and sum
+                new_node = Pathnode(neighbour, current_node)
             # 	- put neighbour in open list
+                open_list.append(new_node)
         # put current cell in closed list
+        closed_list.append(current_node)
         # select cell in open list with lowest g-score
-        # - make this the current node
+        current_node = min(open_list, key=lambda node: node.distance)
 
 
 def get_neighbours(coords):
@@ -39,3 +44,6 @@ class Pathnode:
         diff_x = abs(coords[0] - goal[0])
         diff_y = abs(coords[1] - goal[1])
         return diff_x + diff_y
+
+
+path = pathfind((0, 0), (5, 5))
