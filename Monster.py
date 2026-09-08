efcb83cd7ff036a8fd9de7f4bee36ee753f230a9
AceofJesters <ems.nelson@hotmail.com>
2026-09-08 09:53:36 -0600

    fix player detection :)

--- Monster.py
+++ Monster.py
@@ -4,8 +4,7 @@ from pathfind import pathfind
 MON_KEY = 2
 
 class Monster:
-
-# TODO: Why are you moving down your path backwards that's fucked up stop that
+    #TODO: Fix player detection
 
     def __init__(self, pos=(0, 0), dest=(10, 10), Board=None):
         self.pos = pos
@@ -48,10 +47,11 @@ class Monster:
 
     def check_detection(self, player_pos):
 
-        detection_range = 10
+        detection_range = 7
 
         # find distance between player and monster
         distance = ((self.pos[0] - player_pos[0]) ** 2 + (self.pos[1] - player_pos[1]) ** 2) ** 0.5
 
         if distance <= detection_range:
             self.dest = player_pos
+            return True
